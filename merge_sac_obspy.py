e4a30922d6d18e3c28ba1b2691f4cea178f9ed3a
Mohamadhasan Sarvandani <43719910+Sarvandani@users.noreply.github.com>
2022-12-26 18:39:27 +0100

    Add files via upload

---
+++ merge_sac_obspy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 00:05:06 2020
+Attention: the merged file may need to be deleted after every run!
+@author: Sarvandani
+"""
+import os
+from obspy import read
+from obspy import Stream
+os.chdir ('/volumes/DRIVE_2/Merge_github')
+path = '/volumes/DRIVE_2/Merge_github/'
+RECEIVER_name = "SULZ.LHZ.CH.2013.220.processed"
+##############################################################################################
+st1= Stream()
+for file in os.listdir(path):
+    if file.startswith('.'):
+        continue
+    if RECEIVER_name in file:
+        st1.extend(read(path + file))
+      
+st1.merge(method=1, fill_value=0, interpolation_samples=1)
+# we change the compnent of  receiver => Z, N, E
+st1 = st1.select(channel='*Z')
+print(st1)
+tr = st1[0]
+tr.plot(color='green')  
+tr.write("merged_file.sac", format="sac") 
+out = read("/Volumes/DRIVE_2/Merge_github/merged_file.sac")
+out.plot(color='red')  
+
